86dd236d8bdb1fe8fab6784fe779a2e19bb700d0
Bruno Mota <bmotadev@gmail.com>
2024-08-20 17:57:23 -0300

    :sparkles: feat: Exercicio 1003 - Resolvido

--- src/Exercise1003SomaSimples.java
+++ src/Exercise1003SomaSimples.java
@@ -1,9 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 /**
  * Exercicio 1003 - Soma Simples
- * Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+ * Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. 
+ * A seguir escrever o valor desta variável.
  */
 
 public class Exercise1003SomaSimples {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        int valorA = scanner.nextInt();
+        int valorB = scanner.nextInt();
+
+        int soma = valorA + valorB;
+
+        System.out.printf("SOMA = %d%n", soma);
+
+    
+    }
 }
