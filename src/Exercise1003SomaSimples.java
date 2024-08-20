563ccf26d5d6036fb4e4413aaa3960bdb9239094
Bruno Mota <bmotadev@gmail.com>
2024-08-20 17:41:15 -0300

    :sparkles: feat: Exercicio 1003 - Documentação

---
+++ src/Exercise1003SomaSimples.java
@@ -0,0 +1,9 @@
+
+/**
+ * Exercicio 1003 - Soma Simples
+ * Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+ */
+
+public class Exercise1003SomaSimples {
+
+}
