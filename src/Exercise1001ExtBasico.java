d8e52f92fd8a55c7c6baccc8bb259179e9b34b96
Bruno Mota <bmotadev@gmail.com>
2024-08-19 22:32:22 -0300

    :sparkles: feat: Exercicio 1001 - Resolvido

---
+++ src/Exercise1001ExtBasico.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Exercise1001ExtBasico {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int valorA = scanner.nextInt();
+        int valorB = scanner.nextInt();
+
+        int soma = valorA + valorB;
+
+        System.out.println("X = " + soma);
+
+        
+    }
+}
