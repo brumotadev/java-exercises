cbaa0d4a010b887bb7b6bc5081a52a443fab4a25
Bruno Mota <bmotadev@gmail.com>
2024-08-20 17:36:47 -0300

    :books: docs: Adicionado documentação do exercicio 1001 - Usando o JavaDoc

--- src/Exercise1001ExtBasico.java
+++ src/Exercise1001ExtBasico.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 
+/**
+ * Exercocop 1001 - Extremamente Basico
+ * Leia 2 valores inteiros e armazene-os nas variáveis A e B. 
+ * Efetue a soma de A e B atribuindo o seu resultado na variável X. 
+ * Imprima X
+ * @author Bruno Mota
+ */
 
 public class Exercise1001ExtBasico {
 
