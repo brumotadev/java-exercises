273a30a11c771ef00229da53ac4b5f9e40af49b2
Bruno Mota <bmotadev@gmail.com>
2024-08-21 22:58:19 -0300

    :sparkles: feat: Exercicio 1004 - Documentação

---
+++ src/Exercise1004ProdutoSimples.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise1004ProdutoSimples {
+
+    /**
+     * Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. 
+     * A seguir mostre a variável PROD com mensagem correspondente.
+     * @author Bruno Mota
+     *    
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
