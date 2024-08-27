a6926f1d6a9507a57d2e9dc4efba080039fd9a4c
Bruno Mota <bmotadev@gmail.com>
2024-08-26 21:58:01 -0300

    :recycle: refactor: adicionado docs exercicio 1007

--- src/Exercise1007.java
+++ src/Exercise1007.java
@@ -1,6 +1,13 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercise1007 {
 
+    /**
+     * Leia quatro valores inteiros A, B, C e D. 
+     * A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+     */
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
     }
 }
