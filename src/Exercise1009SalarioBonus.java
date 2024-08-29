ff8a089f18c54ae8a5bf0feeb406493e84d47321
Bruno Mota <bmotadev@gmail.com>
2024-08-28 21:48:53 -0300

    :sparkles: feat: Exercicio 1009 - Documentação

---
+++ src/Exercise1009SalarioBonus.java
@@ -0,0 +1,11 @@
+public class Exercise1009SalarioBonus {
+
+
+    /** 
+     * Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro).
+     * Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês, com duas casas decimais.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
