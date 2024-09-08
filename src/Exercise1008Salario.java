b32ba7a469d320b8d7c7dbe718d8954960668ff3
Bruno Mota <bmotadev@gmail.com>
2024-09-07 22:02:31 -0300

    :sparkles: feat: Exercicio 1008 adicionado scanner

--- src/Exercise1008Salario.java
+++ src/Exercise1008Salario.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Exercise1008Salario {
 
 
@@ -5,6 +7,10 @@ public class Exercise1008Salario {
      * Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello World");
+
+        scanner.close();
     }
 }
