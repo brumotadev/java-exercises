360486b0caf81dc83d889fc60f7140a3915c7397
Bruno Mota <bmotadev@gmail.com>
2024-09-07 22:05:32 -0300

    :sparkles: feat: Exercicio 1013 adicionado scanner

--- src/Exercise1013.java
+++ src/Exercise1013.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Exercise1013 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println();
+
+        scanner.close();
     }
 }
