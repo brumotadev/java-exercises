abc824d039ebe3fbb370a69b2242d2a974d0a46d
Bruno Mota <bmotadev@gmail.com>
2024-09-07 22:04:57 -0300

    :sparkles: feat: Exercicio 1012 adicionado scanner

--- src/Exercise1012Area.java
+++ src/Exercise1012Area.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Exercise1012Area {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println();
+
+        scanner.close();
     }
 }
