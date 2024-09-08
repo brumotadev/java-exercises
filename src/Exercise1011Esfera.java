64f589fd6bdc4850a46bb0070ee28c9a7c8411d2
Bruno Mota <bmotadev@gmail.com>
2024-09-07 22:04:25 -0300

    :sparkles: feat: Exercicio 1011 adicionado scanner

--- src/Exercise1011Esfera.java
+++ src/Exercise1011Esfera.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Exercise1011Esfera {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println();
+
+        scanner.close();
     }
 }
