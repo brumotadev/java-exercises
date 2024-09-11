beb02fbb487d5846792926cffcc70f183798d2b8
Bruno Mota <bmotadev@gmail.com>
2024-09-10 22:25:10 -0300

    :sparkles: feat: Exercicio 1008 - Resolvido

--- src/Exercise1008Salario.java
+++ src/Exercise1008Salario.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercise1008Salario {
@@ -7,9 +8,16 @@ public class Exercise1008Salario {
      * Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Hello World");
+        int numeroFuncionario = scanner.nextInt();
+        int numeroHoras = scanner.nextInt();
+        double valorPorHora = scanner.nextDouble();
+
+        double salario = numeroHoras * valorPorHora;
+
+        System.out.printf("NUMBER = %d\n", numeroFuncionario);
+        System.out.printf("SALARY = U$ %.2f\n", salario);
 
         scanner.close();
     }
