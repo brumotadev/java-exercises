859a2dffc5ac985a7f950b4b328dfc5b5b4e4299
Bruno Mota <bmotadev@gmail.com>
2024-09-11 21:35:35 -0300

    :recycle: refactor: adicionado docs exercicio 1012

--- src/Exercise1012Area.java
+++ src/Exercise1012Area.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 
 public class Exercise1012Area {
 
+    /**
+     * Escreva um programa que leia três valores com ponto flutuante de dupla precisão: 
+     * A, B e C. Em seguida, calcule e mostre:
+     * a) a área do triângulo retângulo que tem A por base e C por altura.
+     * b) a área do círculo de raio C. (pi = 3.14159)
+     * c) a área do trapézio que tem A e B por bases e C por altura.
+     * d) a área do quadrado que tem lado B.
+     * e) a área do retângulo que tem lados A e B.
+     * 
+     */
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
