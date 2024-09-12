67ea4c0439a0948a00145209e9edd581a99d2b46
Bruno Mota <bmotadev@gmail.com>
2024-09-11 21:34:33 -0300

    :recycle: refactor: adicionado docs exercicio 1011

--- src/Exercise1011Esfera.java
+++ src/Exercise1011Esfera.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Exercise1011Esfera {
 
+    /**
+     * Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
+     * 
+     */
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
